c5f620687ef01d210476d1e0d349a683d5fdb7e4
jaluiovilash <ovilashjalui@gmail.com>
2024-09-25 15:30:40 +0530

    Create a Custom URL Shortener

---
+++ short-url/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const URL = require("./models/urls");
+const { connectToMongoDB } = require("./connection")
+const urlRoute = require("./routes/url");
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url")
+    .then(() => console.log("MongoDB connected"))
+
+app.use(express.json())
+
+app.use("/url", urlRoute)
+
+app.get("/:shortId", async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, {
+        $push: {
+            visitHistory: {
+                timestamp: Date.now(),
+            },
+        },
+    });
+    res.redirect(entry.redirectURL);
+})
+
+app.listen(PORT, () => console.log(`server started at PORT: http://localhost:${PORT}`));
